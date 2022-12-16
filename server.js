84301597bdf0f7dd09f82330d8fa3a956c3e0f12
Oscar Ingram <oscardingram@gmail.com>
2022-12-16 09:02:11 -0500

    css works now

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const http = require('http')
-const fs = require('fs')
-const port = 3000
- 
-const server = http.createServer(function(req,res) {
-    res.writeHead(200, {'Content-Type':'text/html'})
-    fs.readFile('main.html', function(error, data){
-        if(error){
-            res.writeHead(404)
-            res.write('Error: File not found')
-        }else{
-            res.write(data)
-        }
-        res.end()
-    })
- 
-})
- 
-server.listen(port, function(error) {
-    if (error) {
-        console.log('An error has occured', error)
- 
-    } else {
-        console.log('Server is running on port ' + port)
-    }
-})
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function(req, res){
+
+    if(req.url === "/"){
+        fs.readFile("main.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
+
+    }else if(req.url.match("\.png$")){
+        var imagePath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "image/png"});
+        fileStream.pipe(res);
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+
+}).listen(3000);
