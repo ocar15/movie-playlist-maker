61945488d58cc03c77d1cffc01baca81db262b31
= <oscar.ingram@polkadotskysoftware.com>
2023-01-07 21:32:46 -0500

    begin writing node class

---
+++ classes.js
@@ -0,0 +1,14 @@
+class Node {
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+
+    constructor() {
+        first = null;
+    }
+
+}
